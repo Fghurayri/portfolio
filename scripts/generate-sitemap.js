955548d12f9f8f1d3fa3542b3d3e62b6ddb396f5
Faisal Alghurayri <fghurayri@gmail.com>
2021-07-22 03:08:06 -0400

    hand roll the sitemap generation since the lib didnt work

---
+++ scripts/generate-sitemap.js
@@ -0,0 +1,38 @@
+import fs from "fs";
+import path from "path";
+import { getPosts, getPostsRawBody } from "../src/lib/posts-importer.js";
+
+async function main() {
+  const postsContent = await getPostsRawBody();
+  const posts = await getPosts(postsContent);
+  createSitemap(posts);
+}
+
+main();
+
+function createSitemap(posts) {
+  const sitemap = `
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" 
+    xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" 
+    xmlns:xhtml="http://www.w3.org/1999/xhtml" 
+    xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" 
+    xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" 
+    xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
+      <url>
+        <loc>https://faisal.sh</loc>
+      </url>
+    ${posts
+      .map(
+        (post) => `
+      <url>
+        <loc>https://faisal.sh/posts/${post.slug}</loc>
+      </url>
+      `
+      )
+      .join("")}  
+  </urlset>
+  `;
+  const __dirname = path.resolve();
+  const location = path.join(__dirname, "static");
+  fs.writeFileSync(path.join(location, "sitemap.xml"), sitemap);
+}
