03b3453e01dafe44a6d292079c204297912fab01
Faisal Alghurayri <fghurayri@gmail.com>
2021-07-24 21:45:15 -0400

    update h2 and h3 tags

--- src/lib/components/tags/h2.svelte
+++ src/lib/components/tags/h2.svelte
@@ -1,5 +1,3 @@
-<h2
-  class="text-xl md:text-3xl font-bold my-8 leading-none tracking-tighter text-gray-500"
->
+<h2 class="text-xl md:text-3xl font-bold my-8  text-gray-500">
   <slot />
 </h2>
--- src/lib/components/tags/h3.svelte
+++ src/lib/components/tags/h3.svelte
@@ -1,5 +1,3 @@
-<h3
-  class="text-lg md:text-xl font-bold my-8 leading-none tracking-tighter text-gray-500"
->
+<h3 class="text-lg md:text-xl font-bold my-8  text-gray-500">
   <slot />
 </h3>
