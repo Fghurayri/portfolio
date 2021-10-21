55478775c4b6067316e2ba96ea202293eb4bc934
Faisal Alghurayri <fghurayri@gmail.com>
2021-10-20 20:55:19 -0400

    update my blog to fix vercel deployment

--- svelte.config.js
+++ svelte.config.js
@@ -1,27 +1,23 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-vercel";
 import { mdsvex } from "mdsvex";
 import mdsvexConfig from "./mdsvex.config.js";
+import preprocess from "svelte-preprocess";
+import adapter from "@sveltejs/adapter-vercel";
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   extensions: [".svelte", ...mdsvexConfig.extensions],
 
   kit: {
-    // hydrate the <div id="svelte"> element in src/app.html
     target: "#svelte",
     adapter: adapter(),
   },
 
   preprocess: [
-    mdsvex(mdsvexConfig),
     preprocess({
       postcss: true,
     }),
+    mdsvex(mdsvexConfig),
   ],
 };
 
 export default config;
-// Workaround until SvelteKit uses Vite 2.3.8 (and it's confirmed to fix the Tailwind JIT problem)
-const mode = process.env.NODE_ENV;
-const dev = mode === "development";
-process.env.TAILWIND_MODE = dev ? "watch" : "build";
